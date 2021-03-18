be0d28860173c1bbf36c1d8dec5aaa2d12e68b2e
tamaspalya <tamas.palya01@gmail.com>
2021-03-18 13:12:16 +0100

    Database classes added

---
+++ src/database/DBSaleOrder.java
@@ -0,0 +1,21 @@
+package database;
+
+import java.util.List;
+
+import model.SaleOrder;
+
+public class DBSaleOrder implements IDBSaleOrder {
+
+	@Override
+	public List<SaleOrder> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SaleOrder findById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
